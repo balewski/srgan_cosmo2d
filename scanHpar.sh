b2d65ddcdda253f5dffd339e984e53e39fd617b8
Jan Balewski <janstar1122@gmail.com>
2022-01-30 14:26:48 -0800

    ok

--- scanHpar.sh
+++ scanHpar.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -u ;  # exit  if you try to use an uninitialized variable
+#set -x ;  # print out the statements as they are being executed
 #set -e ;  #  bash exits if any statement returns a non-true return value
 #set -o errexit ;  # exit if any statement returns a non-true return value
 k=0
@@ -7,24 +8,28 @@ k=0
 #prjquota m3363  # only works on cori
 
 if [ ! -f toolbox/dummy.exe ] ; then
-    echo S: fixe-me1
-    exit
+    echo S: fixe-me1 ;    exit
 fi
 
-mkdir  out
-#for lrFact in 0.02 0.05 0.10 0.20 0.50  1.00 2.00  ; do
-#for lrFact in 0.29 0.44 0.67 1.00 1.50 2.30 3.50  ; do
+nodes=8
+epochs=5700
+design=dev7d
+
+if [ ! -f $design.hpar.yaml  ] ; then
+    echo S: fixe-me2 ;    exit
+fi
+
+
 #for lrFact in  0.69 0.83 1.00 1.20 1.44  0.58  ; do
-for jobId in e f g ; do
+for tag in a b c ; do
     #jobId=lrfG${lrFact}
-
-    echo
+    jobId=N${nodes}_${design}_${tag}
+   
     echo start jobId=$jobId
+    export SRCOS2D_WRK_SUFIX="$jobId"
+    export SRCOS2D_OTHER_PAR=" --epochs ${epochs}  --expName ${jobId} --design $design "  # will overwrite any other settings
 
-    #export SRCOS2D_LR_FACT=$lrFact
-    export SRCOS2D_JOBID=$jobId
-
-    sbatch  batchShifter.slr      # PM
+    sbatch   -N $nodes  batchShifter.slr      # PM
     #./batchShifter.slr      # PM  - interactive
     #bsub  batchSummit.lsf      # Summit
     # ./batchSummit.lsf      # Summit - interactive
@@ -36,3 +41,5 @@ done
 
 echo
 echo SCAN: submitted $k jobs " "`date`
+#Cancel all my jobs:
+#squeue -u $USER -h | awk '{print $1}' | xargs scancel
